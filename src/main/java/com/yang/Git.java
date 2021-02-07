9f5ff466ee8ed6a9f5a63c8c62905ab64188702d
Yxz888 <373233998@qq.com>
2021-02-07 21:13:25 +0800

    version2 提交！

--- src/main/java/com/yang/Git.java
+++ src/main/java/com/yang/Git.java
@@ -8,6 +8,7 @@ public class Git {
 
     public static void main(String[] args) {
         System.out.println("version1:hello git!");
+        System.out.println("version2:hello world!");
 
 
     }
