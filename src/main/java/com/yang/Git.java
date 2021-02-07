ca300ab2bd24e2bba8849e582099a33ee68772a5
Yxz888 <373233998@qq.com>
2021-02-07 20:49:24 +0800

    version1 提交！

---
+++ src/main/java/com/yang/Git.java
@@ -0,0 +1,14 @@
+package com.yang;
+
+/**
+ * @author 你洲哥哥
+ * test git
+ */
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("version1:hello git!");
+
+
+    }
+}
